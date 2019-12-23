19389007371ad2098d6466b826df03ed0f246d43
Marcin Bołba <marcin.bolba@wp.pl>
2019-12-23 23:10:10 +0100

    Add altitude smooth algorithm

--- terrainGenerator/generator.py
+++ terrainGenerator/generator.py
@@ -3,20 +3,21 @@ import matplotlib.pyplot as plt
 
 from tools.pickRandom import PickRandom
 from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
-import matplotlib.pyplot as plt
-import numpy as np
 
 
+# TODO add region avg height report
+# TODO! alg increaseRes favorize one of dimensions
 class Generator:
 
     def __init__(self):
         self.settings = self.Settings()
+        self.terrain = []
 
     class Settings:
         def __init__(self):
             self.minVal = 0
-            self.maxVal = 5
-            self.initialResolution = (5, 5)
+            self.maxVal = 10
+            self.initialResolution = (3,30)
             self.incrementNumber = 5
 
         def set(self, min=1, max=2, resolution=(5, 5), incrementNumber=5):
@@ -26,69 +27,129 @@ class Generator:
             self.incrementNumber = incrementNumber
 
     def generate(self):
-        terrain = np.random.randint(self.settings.minVal, self.settings.maxVal, self.settings.initialResolution)
+        self.terrain = np.random.randint(self.settings.minVal, self.settings.maxVal, self.settings.initialResolution)
         for i in range(self.settings.incrementNumber):
-            terrain = self.__increaseRes(terrain)
-        self.smoothEfect(terrain)
-        self.drawMap(terrain)
+            self.__increaseRes()
+            self.drawSurf()
+        self.__smoothAltitude(10, 5, 0.2)
+        # self.drawMap()
+        # self.drawMap()
+        self.drawSurf()
 
-    def drawMap(self, constMap):
-        plt.matshow(constMap)
+    def drawMap(self):
+        plt.matshow(self.terrain)
+        plt.colorbar()
         plt.show()
 
+    def drawSurf(self):
         fig = plt.figure()
         ax = fig.gca(projection='3d')
-        X = np.arange(0, 129, 1)
-        Y = np.arange(0, 129, 1)
+        X = np.arange(0, len(self.terrain[0]), 1)
+        Y = np.arange(0, len(self.terrain), 1)
         X, Y = np.meshgrid(X, Y)
-        surf = ax.plot_surface(X, Y, constMap)
-
-        fig.colorbar(surf, shrink=0.5, aspect=5)
+        surf = ax.plot_surface(X, Y, self.terrain, cmap='rainbow')
+        # fig.colorbar(surf, shrink=0.5, aspect=5)
         plt.show()
 
-    def __increaseRes(self, constMap):
-        newMap = np.empty([len(constMap) + len(constMap) - 1, len(constMap[0]) + len(constMap[0]) - 1], )
+    def __increaseRes(self):
+        newMap = np.empty([len(self.terrain) + len(self.terrain) - 1, len(self.terrain[0]) + len(self.terrain[0]) - 1])
         newMap[:] = np.nan
 
         # Fill with known values
-        for i in range(len(constMap)):
-            for j in range(len(constMap[0])):
+        for i in range(len(self.terrain)):
+            for j in range(len(self.terrain[0])):
                 ii = i
                 ij = j
                 if i != 0:
                     ii = i * 2
                 if j != 0:
                     ij = j * 2
-                newMap[ii][ij] = constMap[i][j]
+                newMap[ii][ij] = self.terrain[i][j]
 
         # Fill with new values
         for i in range(len(newMap)):
             for j in range(len(newMap[0])):
                 if np.isnan(newMap[i][j]):
                     if i % 2 == 0:
-                        newMap[i][j] = PickRandom.rand2(newMap[i][j - 1], newMap[i][j + 1])
+                        newMap[i][j] = PickRandom.rand3(newMap[i][j - 1], newMap[i][j + 1],
+                                                        (newMap[i][j - 1] + newMap[i][j + 1]) / 2)
                     elif i % 2 == 1:
                         if j % 2 == 0:
-                            newMap[i][j] = PickRandom.rand2(newMap[i - 1][j], newMap[i + 1][j])
+                            newMap[i][j] = PickRandom.rand3(newMap[i - 1][j], newMap[i + 1][j],
+                                                            (newMap[i - 1][j] + newMap[i + 1][j]) / 2)
                         else:
                             # problem z mixem ( od dolu sa NaN-e )
                             newMap[i][j] = PickRandom.rand4(newMap[i - 1][j - 1], newMap[i - 1][j + 1],
                                                             newMap[i + 1][j - 1],
                                                             newMap[i + 1][j + 1])
+        self.terrain = newMap
 
-            # constMap.ass
-        return newMap
+    # smooths surface to remove sharp terrain changes
+    def __smoothSharpEdges(self):
+        for i in range(len(self.terrain)):
+            for j in range(len(self.terrain[0])):
+                if i != 0 and j != 0 and i != len(self.terrain) - 1 and j != len(self.terrain[0]) - 1:
+                    if self.terrain[i - 1][j] == self.terrain[i + 1][j] and self.terrain[i][j - 1] == self.terrain[i][
+                        j + 1]:
+                        self.terrain[i][j] = PickRandom.rand2(self.terrain[i - 1][j], self.terrain[i][j - 1])
+                    elif self.terrain[i - 1][j] == self.terrain[i + 1][j]:
+                        self.terrain[i][j] = self.terrain[i - 1][j]
+                    elif self.terrain[i][j - 1] == self.terrain[i][j + 1]:
+                        self.terrain[i][j] = self.terrain[i][j - 1]
 
-    def smoothEfect(self, constMap):
-        for i in range(len(constMap)):
-            for j in range(len(constMap[0])):
-                if i != 0 and j != 0 and i != len(constMap) - 1 and j != len(constMap[0]) - 1:
-                    if constMap[i - 1][j] == constMap[i + 1][j] and constMap[i][j - 1] == constMap[i][j + 1]:
-                        constMap[i][j] = PickRandom.rand2(constMap[i - 1][j], constMap[i][j - 1])
-                    elif constMap[i - 1][j] == constMap[i + 1][j]:
-                        constMap[i][j] = constMap[i - 1][j]
-                    elif constMap[i][j - 1] == constMap[i][j + 1]:
-                        constMap[i][j] = constMap[i][j - 1]
+    # smooth to avenage terrain changes
+    def __smoothAltitude(self, smoothSize=4, smoothStep=2, smoothRatio=0.25):
+        # parameters
+
+        # helper constants
+        yMax = len(self.terrain)
+        xMax = len(self.terrain[0])
+        smoothsNumberX = np.math.floor((xMax - smoothSize) / smoothStep + 1)
+        smoothsNumberY = np.math.floor((yMax - smoothSize) / smoothStep + 1)
+
+        # guardian block
+        if smoothsNumberX < 1 or smoothsNumberY < 1:
+            print('Cannot perform smooth altitude operation')
+            return
+
+        for i in range(smoothsNumberY):
+            for j in range(smoothsNumberX):
+                # smooth boundaries
+                xStart = j * smoothStep
+                yStart = i * smoothStep
+                xEnd = j * smoothStep + smoothSize
+                yEnd = i * smoothStep + smoothSize
+                # print(self.terrain[yStart:yEnd, xStart:xEnd])
+
+                # calc region avg
+                regionMean = self.terrain[yStart:yEnd, xStart:xEnd].mean()
+                # print('region mean:', regionMean,' reduct:',smoothRatio*regionMean)
+
+                # find bigger / lesser indexes
+                biggerIndexes = np.argwhere(self.terrain[yStart:yEnd, xStart:xEnd] > regionMean)
+                lesserIndexes = np.argwhere(self.terrain[yStart:yEnd, xStart:xEnd] < regionMean)
+
+                # calculate new values
+                maxIncrement = smoothRatio * regionMean
+                for b in range(len(biggerIndexes)):
+                    yIdx = biggerIndexes[b][0] + yStart
+                    xIdx = biggerIndexes[b][1] + xStart
+                    if self.terrain[yIdx, xIdx] - regionMean > maxIncrement:
+                        # max increment
+                        self.terrain[yIdx, xIdx] = self.terrain[yIdx, xIdx] - maxIncrement
+                    else:
+                        # half of remaining increment
+                        self.terrain[yIdx, xIdx] = self.terrain[yIdx, xIdx] - (self.terrain[yIdx, xIdx] - regionMean) / 2
+
+                for b in range(len(lesserIndexes)):
+                    yIdx = lesserIndexes[b][0] + yStart
+                    xIdx = lesserIndexes[b][1] + xStart
+                    if self.terrain[yIdx, xIdx] + maxIncrement < regionMean:
+                        # max increment
+                        self.terrain[yIdx, xIdx] = self.terrain[yIdx, xIdx] + maxIncrement
+                    else:
+                        # half of remaining increment
+                        self.terrain[yIdx, xIdx] = self.terrain[yIdx, xIdx] + (regionMean - self.terrain[yIdx, xIdx]) / 2
 
 
 if __name__ == '__main__':
