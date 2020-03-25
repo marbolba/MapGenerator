4b59a49f09a662c2fc4227aab112292d63f8b26b
Marcin Bołba <marcin.bolba@wp.pl>
2020-03-25 20:35:53 +0100

    Set new manual seed

--- settings/generatorSettings.py
+++ settings/generatorSettings.py
@@ -5,10 +5,19 @@ class GeneratorSettings:
     manualSeed = False
 
     # manual settings
-    manualSeedArray = [[2, 2, 8, 8, 2, 2], [2, 2, 4, 4, 2, 2], [6, 2, 2, 2, 2, 6]]
+    manualSeedArray = [ [2, 2, 2, 2, 2, 2],
+                        [2, 2, 2, 2, 2, 2], 
+                        [8, 8, 8, 8, 2, 2], 
+                        [6, 2, 2, 2, 2, 6], 
+                        [6, 2, 8, 8, 8, 8], 
+                        [6, 2, 2, 2, 2, 6], 
+                        [6, 8, 8, 8, 1, 6], 
+                        [6, 6, 6, 2, 2, 2]]
+
+
     # auto settings
     minVal = 0
-    maxVal = 10
+    maxVal = 5
     initialResolution = (4, 8)
     incrementNumber = 5
 
@@ -17,8 +26,5 @@ class GeneratorSettings:
         if GeneratorSettings.manualSeed:
             return GeneratorSettings.manualSeedArray
         else:
-            return np.random.randint(
-                GeneratorSettings.minVal,
-                GeneratorSettings.maxVal,
-                GeneratorSettings.initialResolution,
+            return np.random.randint(GeneratorSettings.minVal,GeneratorSettings.maxVal,GeneratorSettings.initialResolution
             )
