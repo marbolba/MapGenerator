36102a4585511c278f6a54056e2fd1a93c8e1f54
Marcin Bołba <marcin.bolba@wp.pl>
2020-03-28 18:47:12 +0100

    Add saving accessibility to file

--- main.py
+++ main.py
@@ -9,9 +9,14 @@ if __name__ == "__main__":
     # save results to file
     resultTerrain = g.getTerrain()
     resultAccessibility = g.getAccessibility()
+
+    FileHandler.saveToFile(resultTerrain,"terrain")
+    FileHandler.saveTerrain2D(resultTerrain,"terrain")
+    FileHandler.saveTerrain3D(resultTerrain,"terrain")
+
+    FileHandler.saveToFile(resultAccessibility,"accessibility")
+    FileHandler.saveTerrain2D(resultAccessibility,"accessibility")
+    FileHandler.saveTerrain3D(resultAccessibility,"accessibility")
     
-    FileHandler.saveToFile(resultTerrain)
-    FileHandler.saveTerrain2D(resultTerrain)
-    FileHandler.saveTerrain3D(resultTerrain)
     TerrainDrawer.drawTerrain3D(resultTerrain)
     TerrainDrawer.drawTerrain3D(resultAccessibility)
